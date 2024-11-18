486e6badc860ee36de67bb477e40550ae974f878
Zenith <134686079+Zenithketuaguildepep@users.noreply.github.com>
2024-11-18 09:02:25 +0700

    first commit

---
+++ src/services/getAllProductCategories.js
@@ -0,0 +1,13 @@
+export default function getAllProductCategories(){
+  return  [   {
+      id: 'inikucing',
+      slug: 'kucing oren',
+      name: `kucing`
+    },
+    {
+      id: 'inianjing',
+      slug: 'naek anjing aja',
+      name: `anjing`
+    },
+  ]
+}
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,92 @@
+export const getAllProducts = () => {
+  return [
+    {
+      id: 'inikucing-001',
+      slug: 'kucing oren-001',
+      name: "Kucing Salah Potong",
+      category: `kucing`,
+      categorySlug: 'kucing oren',
+      price: 7_777_777,
+      stock: 777,
+      imageUrl: '/assets/images/Kucing Salah Potong Rambut.jpeg',
+      description: `Kata abang pangkas rambutnya, tunggu late game deck.`
+    },
+    {
+      id: 'inianjing-002',
+      slug: 'naek anjing aja-002',
+      name: "Anjing Bisa Naik Sepeda",
+      category: `anjing`,
+      categorySlug: 'naek anjing aja',
+      price: 7_000_000,
+      stock: 7,
+      imageUrl: `/assets/images/Anjing Bisa Naik Sepeda.jpeg`,
+      description: `Naek anjing aja naek anjing kata gua mah.`
+    },
+    {
+      id: 'inikucing-003',
+      slug: 'kucing oren-003',
+      name: "Sadcing",
+      category: `kucing`,
+      categorySlug: 'kucing oren',
+      price: 6_000_000,
+      stock: 0,
+      imageUrl: '/assets/images/Sadcing.jpeg',
+      description: `Kucing sedih.`
+    },
+    {
+      id: 'inianjing-004',
+      slug: 'naek anjing aja-004',
+      name: "Gojing",
+      category: `anjing`,
+      categorySlug: 'naek anjing aja',
+      price: 5_000_000,
+      stock: 555,
+      imageUrl: `/assets/images/Gojing.jpeg`,
+      description: `Gojing satoru.`
+    },
+    {
+      id: 'inikucing-005',
+      slug: 'kucing oren-005',
+      name: "Smilecing",
+      category: `kucing`,
+      categorySlug: 'kucing oren',
+      price: 4_000_000,
+      stock: 44,
+      imageUrl: '/assets/images/Smilecing.jpeg',
+      description: `Kucing senyum.`
+    },
+    {
+      id: 'inikucing-006',
+      slug: 'kucing oren-006',
+      name: "Hahcing",
+      category: `kucing`,
+      categorySlug: 'kucing oren',
+      price: 3_000_000,
+      stock: 0,
+      imageUrl: '/assets/images/Hahcing.jpeg',
+      description: `Kucing bingung.`
+    },
+    {
+      id: 'inianjing-007',
+      slug: 'naek anjing aja-007',
+      name: "Monkey D Anjing",
+      category: `anjing`,
+      categorySlug: 'naek anjing aja',
+      price: 2_000_000,
+      stock: 0,
+      imageUrl: `/assets/images/Monkey D Anjing.jpeg`,
+      description: `Monkey D Anjing.`
+    },
+    {
+      id: 'inianjing-008',
+      slug: 'naek anjing aja-008',
+      name: "Sidejing",
+      category: `anjing`,
+      categorySlug: 'naek anjing aja',
+      price: 1_000_000,
+      stock: 1,
+      imageUrl: `/assets/images/Sidejing.jpeg`,
+      description: `Anjing side eyes.`
+    },
+  ]
+}
